c6cad65f1aa173ac728d6208fe362e932a3b6264
kwakgeonhyeong <rjsgud1474@naver.com>
2025-11-27 02:39:30 +0900

    fix: analysis.py 수정 & subway.csv 파일명 적용

--- analysis.py
+++ analysis.py
@@ -5,10 +5,10 @@ import matplotlib.pyplot as plt
 df = pd.read_csv("subway.csv", encoding="utf-8")
 
 # 2. 필요한 컬럼만 선택
-# 예시 컬럼: 사용월, 호선명, 역명, 승차총승객수, 하차총승객수
-df = df[["사용월", "호선명", "역명", "승차총승객수", "하차총승객수"]]
+# 사용일자(날짜), 노선명, 역명, 승차총승객수, 하차총승객수
+df = df[["사용일자", "노선명", "역명", "승차총승객수", "하차총승객수"]]
 
-# 3. 역별 총 이용객수 계산 (승차 + 하차)
+# 3. 역별 총 이용객 수 계산
 df["총이용객"] = df["승차총승객수"] + df["하차총승객수"]
 
 # 4. 상위 10개 혼잡역 찾기
@@ -21,6 +21,7 @@ print(top10[["역명", "총이용객"]])
 plt.figure(figsize=(12, 6))
 plt.barh(top10["역명"], top10["총이용객"])
 plt.xlabel("이용객 수")
+plt.ylabel("역 이름")
 plt.title("서울 지하철 혼잡역 TOP 10")
 plt.gca().invert_yaxis()
 plt.show()
